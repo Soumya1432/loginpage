6d1f53bce80e58977ee45f889cd8dd3cfa2fad48
Soumya1432 <biswassoumya17@gmail.com>
2023-09-15 13:42:24 +0530

    ddd

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className={"text-red-700 font-extrabold text-[5rem] font-serif"}>Soham Halder</h1>
+    </div>
+  );
+}
+
+export default App;
